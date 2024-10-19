83520b8a4bd41edc37b5b934a39dc7bf3e87e700
Jacob Deinum <deinum@ualberta.ca>
2024-10-19 00:20:09 -0600

    trees are annoying sometimes

--- 113/path_sum/src/lib.rs
+++ 113/path_sum/src/lib.rs
@@ -20,32 +20,11 @@ impl TreeNode {
     }
 }
 
-type Node = Rc<RefCell<TreeNode>>;
-type Link = Option<Node>;
-type Children = Vec<Link>;
-
-impl TreeNode {
-    pub fn get_left(&self) -> Link {
-        return self.left.clone();
-    }
-
-    pub fn get_right(&self) -> Link {
-        return self.right.clone();
-    }
-
-    pub fn get_value(&self) -> i32 {
-        self.clone().borrow().val
-    }
-
-    pub fn get_children(&self) -> Children {
-        vec![self.get_left(), self.get_right()]
-    }
-}
-
-pub fn make_move(current: &mut Vec<i32>, value: Option<TreeNode>) {
-    match value {
-        None => {}
-        Some(a) => current.push(a.get_value()),
+// making a move consists of adding the node to the current list of nodes
+// Making a move consists of adding the node's value to the current list of nodes
+pub fn make_move(current: &mut Vec<i32>, value: Option<Rc<RefCell<TreeNode>>>) {
+    if let Some(node) = value {
+        current.push(node.clone().borrow_mut().val); // Push the value of the node
     }
 }
 
@@ -53,19 +32,44 @@ pub fn unmake_move(current: &mut Vec<i32>) {
     current.pop();
 }
 
+pub fn get_children(current: Option<Rc<RefCell<TreeNode>>>) -> Vec<Option<Rc<RefCell<TreeNode>>>> {
+    return match current {
+        None => vec![],
+        Some(a) => {
+            let mut res = vec![];
+            res.push(a.clone().borrow_mut().left.clone());
+            res.push(a.clone().borrow_mut().right.clone());
+            res
+        }
+    };
+}
+
 pub fn path_sum_rec(
-    current_node: Node,
+    current_node: Option<Rc<RefCell<TreeNode>>>,
     target_sum: i32,
     current: &mut Vec<i32>,
     acc: &mut Vec<Vec<i32>>,
 ) {
-    if current.iter().sum::<i32>() == target_sum {
+    // we found a path!
+    if current.iter().sum::<i32>() == target_sum
+        && get_children(current_node.clone())
+            .iter()
+            .filter(|x| x.is_some())
+            .count()
+            > 0
+    {
         acc.push(current.clone())
     }
 
-    let x: &TreeNode = current_node.clone().borrow();
+    if current_node.is_none() {
+        return;
+    }
 
-    for node in current_node.clone().borrow().get_children() {
+    let children: Vec<Option<Rc<RefCell<TreeNode>>>> = get_children(current_node);
+    for node in children {
+        if node.is_none() {
+            continue;
+        }
         make_move(current, node.clone());
         path_sum_rec(node, target_sum, current, acc);
         unmake_move(current);
@@ -78,7 +82,7 @@ pub fn path_sum(root: Option<Rc<RefCell<TreeNode>>>, target_sum: i32) -> Vec<Vec
         Some(x) => {
             let mut acc: Vec<Vec<i32>> = Vec::new();
             let mut current: Vec<i32> = vec![];
-            current.push(x.borrow().val);
+            current.push(x.borrow_mut().val);
             path_sum_rec(Some(x), target_sum, &mut current, &mut acc);
             acc
         }
